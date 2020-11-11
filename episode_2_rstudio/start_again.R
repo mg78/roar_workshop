63c0336c0477b4cd37c5a4603826036caeabf821
mg78 <m.groen@lancaster.ac.uk>
2020-11-11 13:56:30 +0000

    Initial commit

---
+++ episode_2_rstudio/start_again.R
@@ -0,0 +1,13 @@
+x <- rnorm(10)
+
+sum <- 0
+
+for(i in 1:length(x)){
+  sum <- sum + x[[i]]
+}
+
+print(sum)
+# save.image() and savehistory() are only needed for this lesson in RStudio cloud for 
+# demonstration purposes - don't put them in your scripts!
+save.image()
+savehistory()
